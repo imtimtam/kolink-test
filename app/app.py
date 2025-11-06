1721ce1d1cf8e05fb466d4131d21b92272b4b0b5
Timothy Tam <imtimtam@gmail.com>
2025-11-05 20:27:05 -0800

    remove test route

--- app/app.py
+++ app/app.py
@@ -3,7 +3,3 @@ from app.routes import pubmed_routes
 
 app = FastAPI()
 app.include_router(pubmed_routes.router, prefix="/pubmed")
-
-@app.get("/")
-def test():
-    return {"message": "API is working!"}
