2be7a31beb695f05e03ce2004aeb10f24abd8223
Timothy Tam <imtimtam@gmail.com>
2025-11-06 20:44:41 -0800

    feat: add testing to clinicaltrials pipeline

--- app/services/clinicaltrials_services.py
+++ app/services/clinicaltrials_services.py
@@ -1,19 +1,41 @@
 from fastapi import FastAPI, HTTPException
+import requests
 
 app = FastAPI()
 
-PUBMED_BASE_URL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+CT_BASE_URL = "https://clinicaltrials.gov/api/v2/"
+STUDIES_ENDPOINT = CT_BASE_URL + "/studies"
 
-@app.get("/clinical-trials")
-def get_clinical_trials():
-    # Logic to retrieve clinical trials
-    return {"message": "List of clinical trials"}
+STATUS_GROUPS = {
+    "active": ["ACTIVE_NOT_RECRUITING", "ENROLLING_BY_INVITATION", "NOT_YET_RECRUITING", "RECRUITING"],
+    "completed": ["COMPLETED", "APPROVED_FOR_MARKETING", "AVAILABLE"],
+    "paused": ["SUSPENDED", "TEMPORARILY_NOT_AVAILABLE"],
+    "terminated": ["TERMINATED", "WITHDRAWN", "NO_LONGER_AVAILABLE", "WITHHELD"]
+}
 
-@app.get("/clinical-trials/{trial_id}")
-def get_clinical_trial(trial_id: int):
-    # Logic to retrieve a specific clinical trial
-    if trial_id == 1:
-        return {"message": "Details of clinical trial 1"}
-    else:
-        raise HTTPException(status_code=404, detail="Clinical trial not found")
- 
+def get_cts(term: str | None = None, status: str | list[str]= "completed"):
+    term = term or ""
+
+    if isinstance(status, str):
+        status = [status]
+    overall_status = []
+    for s in status:
+        s_lower = s.lower()
+        if s_lower in STATUS_GROUPS:
+            overall_status.extend(STATUS_GROUPS.get(s_lower, []))
+        else:
+            overall_status.append(s.upper())
+
+    params = {
+        "format": "json",
+        "query.term": term,
+        "filter.overallStatus": ",".join(overall_status),
+        "pageSize": 1
+    }
+    r = requests.get(STUDIES_ENDPOINT, params=params)
+    r.raise_for_status()
+    return r.json()
+
+if __name__ == "__main__":
+    print(get_cts(term="diabetes", status=["paused", "terminated"]))
+    #print(get_cts_on_condition("diabetes"))
