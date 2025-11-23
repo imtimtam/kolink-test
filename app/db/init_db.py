5d446dad2ba69f105fe6f759130269f48540265d
Timothy Tam <imtimtam@gmail.com>
2025-11-23 15:25:31 -0800

    feat: update to 2.0 SQLAlchemy standards

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,5 +1,7 @@
 from .session import engine
-from .models import pubmed, clinicaltrials
+from .models import PubMed, ClinicalTrials
 from .base import Base
 
+# JUST NEED TO CALL ONCE TO CREATE TABLES
+# python -m app.db.init_db
 Base.metadata.create_all(bind=engine)
