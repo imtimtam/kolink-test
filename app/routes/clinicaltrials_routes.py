0cc0f473ffd0ea39c0e2d24f2dffe801ebdc9b30
Timothy Tam <imtimtam@gmail.com>
2025-11-24 17:05:37 -0800

    feat: create api routes for search and profile endpoints

---
+++ app/routes/clinicaltrials_routes.py
@@ -0,0 +1,76 @@
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy import func, select, or_
+from datetime import date
+from typing import List
+
+from app.routes.clinicaltrials_models import ClinicalTrialsSearchResponse
+from app.db.session import get_db
+from app.db.models import ClinicalTrials
+
+# uvicorn app.routes.clinicaltrials_routes:app --reload
+app = FastAPI()
+
+@app.get("/clinicaltrials", response_model=List[ClinicalTrialsSearchResponse])
+def search_clinicaltrials(
+    term: str | None = None,
+    nct_id: str | None = None,
+    org_name: str | None = None,
+    lead_sponsor: str | None = None,
+    conditions: str | None = None,
+    study_type: str | None = None,
+    phase: str | None = None,
+    status: str | None = None,
+    start_date: date | None = None,
+    completion_date: date | None = None,
+    db: Session = Depends(get_db)
+):
+    stmt = select(ClinicalTrials)
+    filters = []
+
+    if nct_id:
+        filters.append(ClinicalTrials.nct_id == nct_id)
+    if org_name:
+        filters.append(ClinicalTrials.org_name.ilike(f"%{org_name}%"))
+    if lead_sponsor:
+        filters.append(ClinicalTrials.lead_sponsor.ilike(f"%{lead_sponsor}%"))
+    if conditions:
+        filters.append(func.array_to_string(ClinicalTrials.conditions, '||').ilike(f"%{conditions}%"))
+    if study_type:
+        filters.append(ClinicalTrials.study_type.ilike(f"%{study_type}%"))
+    if phase:
+        filters.append(func.array_to_string(ClinicalTrials.phase, '||').ilike(f"%{phase}%"))
+    if status:
+        filters.append(ClinicalTrials.status.ilike(f"%{status}%"))
+    if start_date:
+        filters.append(ClinicalTrials.start_date >= start_date)
+    if completion_date:
+        filters.append(ClinicalTrials.completion_date <= completion_date)
+    
+    if term:
+        filters.append(or_(
+            ClinicalTrials.official_title.ilike(f"%{term}%"),
+            ClinicalTrials.brief_title.ilike(f"%{term}%"),
+            ClinicalTrials.brief_summary.ilike(f"%{term}%"),
+            func.array_to_string(ClinicalTrials.conditions, "||").ilike(f"%{term}%"),
+            func.array_to_string(ClinicalTrials.keywords, "||").ilike(f"%{term}%"),
+        ))
+
+    if filters:
+        stmt = stmt.where(*filters).limit(5)
+
+    results = db.scalars(stmt).all()
+    if not results:
+        raise HTTPException(status_code=404, detail="No matching Clinical Trials found.")
+    return [ClinicalTrialsSearchResponse.model_validate(result) for result in results]
+
+@app.get("/clinicaltrials/{nct_id}", response_model=ClinicalTrialsSearchResponse)
+def search_clinicaltrials(
+    nct_id: str,
+    db: Session = Depends(get_db)
+):
+    stmt = select(ClinicalTrials).where(ClinicalTrials.nct_id == nct_id)
+    results = db.scalars(stmt).first()
+    if not results:
+        raise HTTPException(status_code=404, detail="No matching Clinical Trials found.")
+    return ClinicalTrialsSearchResponse.model_validate(results)
--- app/routes/pubmed_routes.py
+++ app/routes/pubmed_routes.py
@@ -1,11 +1,66 @@
-from fastapi import APIRouter
-from app.services.pubmed.api_pipeline import pubmed_services
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy import func, select, or_
+from datetime import date
+from typing import List
 
-router = APIRouter()
+from app.routes.pubmed_models import PubMedSearchResponse
+from app.db.session import get_db
+from app.db.models import PubMed
 
-@router.get("/search/{term}")
-def get_author_articles(term: str):
-    pmids = pubmed_services.search(term)
-    xml_data = pubmed_services.fetch_pubmed_metadata(pmids)
-    parsed_data = pubmed_services.parse_pubmed_xml_to_json(xml_data)
-    return parsed_data
+# uvicorn app.routes.pubmed_routes:app --reload
+app = FastAPI()
+
+@app.get("/pubmed", response_model=List[PubMedSearchResponse])
+def search_pubmed(
+    term: str | None = None,
+    pmid: int | None = None,
+    publication_type: str | None = None,
+    author: str | None = None,
+    start_date: date | None = None,
+    end_date: date | None = None,
+    language: str | None = None,
+    db: Session = Depends(get_db)
+):
+    stmt = select(PubMed)
+    filters = []
+
+    if pmid:
+        filters.append(PubMed.pmid == pmid)
+    if publication_type:
+        filters.append(func.array_to_string(PubMed.publication_types, '||').ilike(f"%{publication_type}%"))
+    if author:
+        filters.append(PubMed.authors.ilike(f"%{author}%"))
+    if start_date:
+        filters.append(PubMed.date_published >= start_date)
+    if end_date:
+        filters.append(PubMed.date_published <= end_date)
+    if language:
+        filters.append(PubMed.language.ilike(f"%{language}%"))
+    
+    if term:
+        filters.append(or_(
+            PubMed.title.ilike(f"%{term}%"),
+            PubMed.journal_title.ilike(f"%{term}%"),
+            PubMed.abstract.ilike(f"%{term}%"),
+            func.array_to_string(PubMed.mesh_terms, '||').ilike(f"%{term}%")
+        ))
+
+    if filters:
+        stmt = stmt.where(*filters).limit(5)
+
+    results = db.scalars(stmt).all()
+    if not results:
+        raise HTTPException(status_code=404, detail="No matching PubMed articles found.")
+    return [PubMedSearchResponse.model_validate(result) for result in results]
+
+@app.get("/pubmed/{pmid}", response_model=PubMedSearchResponse)
+def search_pubmed(
+    pmid: int,
+    db: Session = Depends(get_db)
+):
+    stmt = select(PubMed).where(PubMed.pmid == pmid)
+    results = db.scalars(stmt).first()
+    if not results:
+        raise HTTPException(status_code=404, detail="No matching PubMed articles found.")
+    return PubMedSearchResponse.model_validate(results)
